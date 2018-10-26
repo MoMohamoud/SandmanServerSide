4ff2521ef965d482931098c68a34915a74587fb8
Mahamoud <mmohamoud@dosarrest.com>
2018-10-26 06:15:19 -0700

    minor update

---
+++ README.md
@@ -0,0 +1,12 @@
+# Launching Sandman
+
+#### First Time Running the application:
+This will install the packages and run the application
+> npm run sandman
+
+#### Run the application using:
+If packages already installed, just start the application
+> npm start
+
+### Tech Stack
+express
--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const request = require('request-promise');
 const port = 8000;
-const keyValue =  "KFL3hYZyIAOiggufyTGD4FVYtLb0vE33";
+const keyValue = "KFL3hYZyIAOiggufyTGD4FVYtLb0vE33";
 let apiUrl = `https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=${keyValue}`;
-const example = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=KFL3hYZyIAOiggufyTGD4FVYtLb0vE33&location=BOS&check_in=2018-12-15&check_out=2018-12-16";
+
+app.use(cors())
 
 // Make sure your output only contains the required information, 
 //the name of the hotel, address, phone number, and the rate for a room.
-const requestDetails = (values) => {
-    return new Promise ((resolve, reject) => {
+const requestDetails = (searchParams) => {
+  return new Promise((resolve, reject) => {
     const reqOptions = {
-        uri: example,
-        json: true 
+      uri: `${apiUrl}${searchParams}`,
+      json: true
     };
     request(reqOptions)
-    .then(function (response) {
+      .then(function (response) {
         const filteredHotelInfo = [];
-        response.results.map( (hotel) => {
-            const hotelObj = {
-                name: hotel.property_name,
-                address: hotel.address,
-                price: hotel.total_price,
-                phoneNumber: hotel.contacts
-            }
-            filteredHotelInfo.push(hotelObj)
+        response.results.map((hotel, index) => {
+          const hotelObj = {
+            id: index,
+            name: hotel.property_name,
+            address: hotel.address,
+            price: hotel.total_price,
+            phoneNumber: hotel.contacts,
+            location: hotel.location
+          }
+          filteredHotelInfo.push(hotelObj)
         })
         resolve(filteredHotelInfo)
-    })
-    .catch(function (err) {
+      })
+      .catch(function (err) {
         reject(err)
-    });
-        })
+      });
+  })
 };
 
 
 
 app.get('/', (req, res) => {
-    if(req && req.query && req.query.airport === undefined || req.query.startDate === undefined) {
-        return res.status(404).send("Missing Required Params");
-        }
-    const location = {
+  if (req && req.query && req.query.airport === undefined || req.query.startDate === undefined || req.query.endDate === undefined) {
+    return res.status(404).send("Missing Required Params");
+  }
 
-    };
-    const date = req.query.startDate;
-    const constructParams = `&location=${req.query.airport}&check_in=${req.query.startDate}&check_out=${req.query.endDate}`
+  const constructParams = `&location=${req.query.airport}&check_in=${req.query.startDate}&check_out=${req.query.endDate}`
 
-    requestDetails(location).then( (response) => {
-        return res.json(response)
-
-    }).catch( (err) => {
-        console.log(err)&location=BOS
-        return res.status(404).send("API Error");
-    })
-    console.log('teste', req.query)
+  requestDetails(constructParams).then((response) => {
+    return res.json(response)
+  }).catch((err) => {
+    return res.status(404).send("API Error");
+  })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`SandMan app listening on port ${port}!`))
